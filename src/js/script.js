77d9a179befc44703d6dfa3a213c10c865ca3078
Nikita <Nikita@gmail.com>
2021-09-25 13:31:39 +0700

    create modal window

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    //slider
     $('.carousel__inner').slick({
         autoplay: true,
         autoplaySpedd: 2000,
@@ -6,6 +8,7 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/fourth_screen/slider/chevron-right-solid.png"></button>',
     });
 
+    //tabs
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -25,6 +28,26 @@ $(document).ready(function(){
             e.preventDefault();
             $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    })
+
+    //modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.catalog-item__btn').on('click', function () {
+        $('.overlay, #buing').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #buing, #thanks').fadeOut('slow');
+    })
+
+    $('.catalog-item__btn').each(function (i) {
+        $(this).on('click', function () {
+            $('#buing .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #buing').fadeIn('slow'); 
         })
     })
 });
